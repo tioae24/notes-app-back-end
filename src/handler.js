761e8e74fcb2ccb7b7cb7a148289f04b8b1b7a64
tyo <tyoabdi24@gmail.com>
2022-07-19 20:50:09 +0700

    fix bugs host value

--- src/handler.js
+++ src/handler.js
@@ -104,9 +104,9 @@ const addNoteHandler = (request, h) => {
   const deleteNoteByIdHandler = (request, h) => {
     const {id} = request.params;
 
-    const index = notes.findIndex((note) => note.id ===id);
+    const index = notes.findIndex((note) => note.id === id);
 
-    if(index === -1){
+    if(index !== -1){
       notes.splice(index, 1);
       const response = h.response({
         status: 'success',
